8fdf79adbb68782e33743d213ee3a6800ec95214
Jiaqi Li <lijiaqigreat@gmail.com>
2026-03-26 20:25:55 -0700

    refactor: replace 4-method Game interface with generator-based play()

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,10 +1,11 @@
 import type { ZodSchema } from 'zod'
 import type { GameResponse, GameConfig, GameOutcome } from './types.js'
 
+export type PlayerAction = { playerId: string; action: unknown }
+
+export type GameFlow = Generator<GameResponse, GameOutcome, PlayerAction>
+
 export interface Game {
   readonly optionsSchema: ZodSchema
-  init(config: GameConfig): Promise<GameResponse> | GameResponse
-  handleResponse(playerId: string, action: unknown): Promise<GameResponse> | GameResponse
-  isTerminal(): boolean
-  getOutcome(): GameOutcome | null
+  play(config: GameConfig): GameFlow
 }
